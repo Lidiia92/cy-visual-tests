1e076f75a4c9202af80f956c74831238b23ce888
Lidii <lidiiany92@gmail.com>
2020-04-23 14:23:51 -0700

    date and time override

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -1,9 +1,17 @@
-describe('Visual Regression', () => {
-	it('My first visual regression test', () => {
-		//load website
-		cy.visit('http://example.com');
+const pages = ['http://example.com'];
 
-		//compare snapshots
-		cy.matchImageSnapshot();
+const sizes = ['iphone-6', 'ipad-2', [1200, 800]];
+
+describe('Visual Regression', () => {
+	sizes.forEach((size) => {
+		pages.forEach((page) => {
+			it(`should match ${page} in resolution of ${size}`, () => {
+				let currentTime = new Date(Date.UTC(2020, 1, 1)).getDate();
+				cy.clock(currentTime);
+				cy.setResolution(size);
+				cy.visit(page);
+				cy.matchImageSnapshot();
+			});
+		});
 	});
 });
